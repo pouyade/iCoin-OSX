07a9d62abc4700182e23240c672b595079ca8b42
Pouya Dark <pouya@pouyas-macbook-pro.local>
2018-06-24 16:26:12 +0430

    Initial Commit

---
+++ BitMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BitMac
+//
+//  Created by pouya  on 6/24/18.
+//  Copyright © 2018 DarkSoft. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
